93387d8e205930dcee147467256d025356f2a75a
Nicole Sattler <nicole.sattler@live.com>
2017-06-22 16:58:31 +0000

    Modified contact section. Added contact information, styles and click functions to view information

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
 /* global $ */
 
-$(document).ready(function(){
+/*
+TODO
+
+Add 'back to top' button
+
+*/
+
+$(document).ready(function() {
     
-    $('#contactBtn').on('click', function(){
-       $('html, body').animate({
-      scrollTop: $('.contact').offset().top
-    },1000);
+    
+    //Scroll to contact section
+    $('#contactBtn').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $('.contact').offset().top
+        }, 1000);
+    });
+
+    //Scroll to info section
+    $('#learnMore').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $('.third').offset().top
+        }, 1000);
     });
     
-    $('#learnMore').on('click', function(){
-       $('html, body').animate({
-      scrollTop: $('.third').offset().top
-    },1000);
+    
+    //Contact section click functions
+    $('.fa-facebook').on('click', function() {
+        var $this = $('.fa-facebook');
+        switchActive($this);
+        $('.contactViews').find('.fb').removeClass('hidden').addClass('showing');
     });
+
+    $('.fa-phone').on('click', function() {
+        var $this = $('.fa-phone');
+        switchActive($this);
+        $('.contactViews').find('.phone').removeClass('hidden').addClass('showing');
+    });
+
+    $('.fa-whatsapp').on('click', function() {
+        var $this = $('.fa-whatsapp');
+        switchActive($this);
+        $('.contactViews').find('.whats').removeClass('hidden').addClass('showing');
+    });
+
+
 });
+
+// Function to switch between active/inactive
+function switchActive(toSwitch) {
+    toSwitch.parent().find('.fa').removeClass('active').addClass('inactive');
+    toSwitch.removeClass('inactive').addClass('active');
+    $('.contactViews').find('span').removeClass('showing').addClass('hidden');
+}
